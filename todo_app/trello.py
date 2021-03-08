0d42c7e8c0b71d883ad8fca77be6e2bf09835b84
Luke Stern <luke.stern@me.com>
2021-03-08 13:31:45 +0000

    Trello api methods

--- todo_app/trello.py
+++ todo_app/trello.py
@@ -1,3 +1,4 @@
+from types import MethodDescriptorType
 from dotenv import load_dotenv
 import requests
 import itertools
@@ -13,17 +14,21 @@ class Trello:
         self.url = os.environ.get('TRELLO_BASE_URL')
         self.board = os.environ.get('TRELLO_BOARD_ID')
         self.username = os.environ.get('TRELLO_USER')
+        self.list_ids = self.get_card_list_ids()
 
-    def get_and_check_response(self, url, method='get', body=None):
+    def make_api_call_and_check_response(self, url, method='get', data=None):
         complete_url = f"{self.url}" + url + f"?key={self.key}&token={self.token}"
-        resp = eval("requests." + method.lower() + "('" + complete_url + "')")
+        resp = eval("requests." + method.lower() + "('" + complete_url + "', data=" + str(data) + ")")
         if resp.status_code == 200:
-            # return json.dumps(resp.json(), indent=4)
+            # print(json.dumps(resp.json(), indent=4))
             return resp.json()
 
     def get_card_list_ids(self):
+        '''
+        Returns a dictionary where keys are the list names and the values are list ids.
+        '''
         url = f"boards/{self.board}/lists"
-        result = self.get_and_check_response(url)
+        result = self.make_api_call_and_check_response(url)
         if result:
             lists = {}
             for lis in result:
@@ -31,14 +36,38 @@ class Trello:
             return  lists
 
     def get_cards_from_a_list(self, list_id):
+        '''
+        Returns list of card dictionaries.
+        '''
         url = f"lists/{list_id}/cards"
-        return self.get_and_check_response(url)
+        result = self.make_api_call_and_check_response(url)
+        cards = []
+        for res in result:
+            cards.append(dict((k, res[k]) for k in ('id', 'name', 'idList') if k in res))
+        return cards
 
     def get_all_cards(self):
-        list_ids = self.get_card_list_ids()
-        cards_in_lists = [self.get_cards_from_a_list(list_ids[key]) for key in list_ids]
-        return list(itertools.chain.from_iterable(cards_in_lists))
+        cards_in_lists = [self.get_cards_from_a_list(self.list_ids[key]) for key in self.list_ids]
+        card_list = list(itertools.chain.from_iterable(cards_in_lists))
+        return self.add_status_to_cards_in_list(card_list)
 
-    def update_card(self, card_id, update_dictionary):
+    def add_status_to_cards_in_list(self, card_list):
+        for card in card_list:
+            for key, value in self.list_ids.items():
+                if card['idList'] == value:
+                    card['status'] = key
+        return card_list
+
+    def move_card_to_list(self, card_id, card_status):
+        data = dict(idList = self.list_ids[card_status])
         url = f"cards/{card_id}"
-        return self.get_and_check_response(url)
+        return self.make_api_call_and_check_response(url, method='put', data=data)
+
+    def create_new_card(self, name):
+        data = dict(idList = self.list_ids['Not Started'], name = name)
+        url = f"cards"
+        return self.make_api_call_and_check_response(url, method='post', data=data)
+
+    def delete_card(self, card_id):
+        url = f"cards/{card_id}"
+        return self.make_api_call_and_check_response(url, method='delete')
