d9aa6147cb11e916a421dd3a65b244e50d7b7e0a
Luke Stern <luke.stern@softwire.com>
2021-10-29 11:57:31 +0100

    Using .netrc

--- heroku-deploy.sh
+++ heroku-deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+mv /app/.netrc ~/.netrc
+
 heroku container:login
 docker tag "${DOCKER_USER}"/todo_app registry.heroku.com/"${HEROKU_APP}"/web
 docker login --username=_ --password="${HEROKU_KEY}" registry.heroku.com
