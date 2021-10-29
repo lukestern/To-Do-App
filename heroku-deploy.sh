ffda8ea87570912989c0043fde2f5562b2fa8c11
Luke Stern <luke.stern@softwire.com>
2021-10-29 12:22:47 +0100

    Move script into travis.yml

--- heroku-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-heroku container:login
-docker tag "${DOCKER_USER}"/todo_app registry.heroku.com/"${HEROKU_APP}"/web
-echo $DOCKER_PASSWD | docker login --username $DOCKER_USER --password-stdin
-docker push registry.heroku.com/"${HEROKU_APP}"/web
-heroku container:release web --app "${HEROKU_APP}"
