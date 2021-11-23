c3ab9348bdaff4728a332c91f4f29dc0ecb921cc
Luke Stern <luke.stern@softwire.com>
2021-11-23 16:32:11 +0000

    Typo

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-docker tag $DOCKER_USER/todo_app registry.heroku.com/$HEROKU_APP/web
+docker tag $DOCKER_USER/todo-app registry.heroku.com/$HEROKU_APP/web
 heroku container:login
 docker push registry.heroku.com/$HEROKU_APP/web
 heroku container:release web -a $HEROKU_APP
